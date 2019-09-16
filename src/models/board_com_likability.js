0f8f9d42ddca8422926c3880153e90d33547d326
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2019-09-16 23:03:41 +0900

    댓글 호불호 모델링

---
+++ src/models/board_com_likability.js
@@ -0,0 +1,18 @@
+export const board_com_likability = (sequelize, DataTypes) => {
+    return sequelize.define(board_com_likability,{
+        comment_id : {
+            type : DataTypes.INTEGER,
+            primaryKey : true,
+            auto_increment : true
+        },
+        user_id : {
+            type : DataTypes.INTEGER,
+            primaryKey : true,
+            auto_increment : true
+        },
+        likability : {
+            type : DataTypes.ENUM("좋아요","싫어요"),
+            allowNull : false
+        }
+    })
+}
