86b31da031b84a3135b49acad57d0d209314bfd6
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-17 23:44:36 +0900

    다음 호실 신청 데이터 검증까지

--- src/api/dormitory/room/room.ctrl.js
+++ src/api/dormitory/room/room.ctrl.js
@@ -7,3 +7,22 @@ import { decodeToken } from '../../../lib/token.js';
 // .env 파일의 환경 변수 불러오기
 import dotenv from 'dotenv';
 dotenv.config();
+
+export const ApplyNextRoom = async (ctx) => {
+    const Application = Joi.object().keys({
+        students: Joi.array().items(Joi.number().integer()).min(1).required()
+    })
+    
+    const validation = Joi.validate(ctx.request.body, Application);
+    
+    if (validation.error) {
+        console.log("RegStudent - Joi 형식 에러");
+        ctx.status = 400;
+        ctx.body = {
+            "error" : "002"
+        };
+        return;
+    }
+    
+    
+};
