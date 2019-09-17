14ae93077d139cc4861954d649e2581ead328103
hmoon630 <s19017@gsm.hs.kr>
2019-09-17 09:08:23 +0900

    Setting to coding

--- src/api/dormitory/index.js
+++ src/api/dormitory/index.js
@@ -2,10 +2,9 @@ import Router from 'koa-router';
 import {} from './point.ctrl';
 import {} from './request_song.ctrl';
 import {} from './room.ctrl';
-import {BorrowLaptop} from './self_study.ctrl';
+import {} from './self_study.ctrl';
 
 const dormitory = new Router();
 
-dormitory.post('/borrow', BorrowLaptop);
 
 export default dormitory;
