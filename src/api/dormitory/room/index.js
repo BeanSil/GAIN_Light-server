1e009924669228d558b4bb5393ea1b1fbdaefa91
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-17 23:44:08 +0900

    Room 라우팅 방식 교체

---
+++ src/api/dormitory/room/index.js
@@ -0,0 +1,8 @@
+import Router from 'koa-router';
+import {} from './room.ctrl';
+
+const room = new Router();
+
+
+
+export default room;
---
+++ src/api/dormitory/room/room.ctrl.js
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+import { account, room, roomApply } from '../../../models';
+
+import { decodeToken } from '../../../lib/token.js';
+
+// .env 파일의 환경 변수 불러오기
+import dotenv from 'dotenv';
+dotenv.config();
