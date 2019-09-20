0faecc72522211505a480b14fe8319ba9eac7f89
DaeHun <skypedanny@naver.com>
2019-09-20 17:47:13 +0900

    인덱스 수정

--- src/api/dormitory/index.js
+++ src/api/dormitory/index.js
@@ -1,9 +1,14 @@
 import Router from 'koa-router';
 import {} from './point.ctrl';
 import {} from './request_song.ctrl';
+import { SongList, SongRequest, SongModify, SongDelete } from './request_song.ctrl';
 import {} from './room.ctrl';
 import {} from './self_study.ctrl';
 
 const dormitory = new Router();
+dormitory.post("/song", SongRequest);
+dormitory.get("/song", SongList);
+dormitory.put("/song", SongModify);
+dormitory.delete("/song", SongDelete);
 
 export default dormitory;
