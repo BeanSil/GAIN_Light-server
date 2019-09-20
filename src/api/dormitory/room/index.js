c7daf1488cfe9e8de59613c3997cd4b6a3161a80
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-20 12:04:13 +0900

    [ADD] 기능 Router 연결

--- src/api/dormitory/room/index.js
+++ src/api/dormitory/room/index.js
@@ -1,5 +1,5 @@
 import Router from 'koa-router';
-import {ApplyNextRoom, SetRoom, UpdateNextRoom, GetRooms, GetRoomByRoomId, GetRoomByUserId, UserVerification} from './room.ctrl';
+import {ApplyNextRoom, SetRoom, UpdateNextRoom, GetRooms, GetRoomByRoomId, GetRoomByUserId, GetApplication, GetApplicationByUserId, GetApplications, DeleteApplication, UserVerification} from './room.ctrl';
 
 import { jwtMiddleware } from '../../../lib/token.js';
 
@@ -9,9 +9,16 @@ room.use(jwtMiddleware);
 
 room.use(UserVerification);
 
+room.get('/apply', GetApplications);
 room.post('/apply', ApplyNextRoom);
+room.get('/apply/all', GetApplications);
+room.get('/apply/:apply_id', GetApplication);
+room.get('/apply/:apply_id/apply_id', GetApplication);
+room.get('/apply/:user_id/user_id', GetApplicationByUserId);
 room.post('/apply/:apply_id', UpdateNextRoom);
+room.delete('/apply/:apply_id', DeleteApplication);
 room.put('/:room_id', SetRoom);
+room.get('/', GetRooms);
 room.get('/all', GetRooms);
 room.get('/:room_no', GetRoomByRoomId);
 room.get('/:room_no/room_no', GetRoomByRoomId);
