e60a5544f3e41dce0bea5699ed0d945568103f66
hmoon630 <s19017@gsm.hs.kr>
2019-09-20 13:55:01 +0900

    Update index.js

--- src/api/dormitory/index.js
+++ src/api/dormitory/index.js
@@ -2,8 +2,14 @@ import Router from 'koa-router';
 import {} from './point.ctrl';
 import {} from './request_song.ctrl';
 import {} from './room.ctrl';
-import {} from './self_study.ctrl';
+import { LrnReserve, LrnCancel, LrnList } from './self_study.ctrl';
 
 const dormitory = new Router();
+
+dormitory.post('/selfstudy', LrnReserve);
+
+dormitory.get('/selfstudy', LrnList)
+
+dormitory.delete('/selfstudy', LrnCancel);
 
 export default dormitory;
--- src/models/index.js
+++ src/models/index.js
@@ -6,6 +6,7 @@ import { Student } from './Student';
 import { Teacher } from './Teacher';
 import { Guest } from './Guest';
 import { Board } from './Board';
+import { Lrnrooms, Lrnseats, Lrnseat_recommend} from './Lrnrooms';
 
 const config = require(path.join(__dirname, '..', 'config', 'dbconfig.json'))['gain'];
 
@@ -21,5 +22,9 @@ const student = Student(sequelize, Sequelize);
 const teacher = Teacher(sequelize, Sequelize);
 const guest = Guest(sequelize, Sequelize);
 const board = Board(sequelize, Sequelize);
+const lrnrooms = Lrnrooms(sequelize, Sequelize);
+const lrnseats = Lrnseats(sequelize, Sequelize);
+const lrnseat_recommend = Lrnseat_recommend(sequelize, Sequelize);
 
-export { sequelize, Sequelize, account, student, teacher, guest, board };
+
+export { sequelize, Sequelize, account, student, teacher, guest, board, lrnrooms, lrnseats, lrnseat_recommend};
