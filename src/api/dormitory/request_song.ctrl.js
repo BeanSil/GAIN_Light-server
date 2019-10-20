28ae46b5a8126636f040729916eb0d3504aa8739
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-10-20 13:46:23 +0900

    [UPDATE] Code Practice Stuff

--- src/api/dormitory/request_song.ctrl.js
+++ src/api/dormitory/request_song.ctrl.js
@@ -14,11 +14,11 @@ export const SongRequest = async (ctx) => {
     //비교 후 에러발생시 400에러코드 전송, body에 001로 오류임을 알린다.
     
     if(Result.error) {
-        console.log("SongRequest - Joi 형식 에러")
+        console.log("SongRequest - Joi 형식 에러");
         ctx.status = 400;
         ctx.body = {
             "error" : "001"
-        }
+        };
         return;
     }
 
@@ -33,12 +33,12 @@ export const SongRequest = async (ctx) => {
         }
     });
 
-    if(founded.auth != '학생') {
+    if(founded.auth !== '학생') {
         console.log(`SongRequest - 학생이 아닙니다.`);
         ctx.status = 400;
         ctx.body = {
             "error" : "some errorcode"
-        }
+        };
         return;
     }
 
@@ -50,13 +50,13 @@ export const SongRequest = async (ctx) => {
         "user_id" : user.user_id
     });
 
-    console.log(`SongRequest - 노래 신청이 완료되었습니다. 노래이름 : ${ctx.request.body.song_name}`)
+    console.log(`SongRequest - 노래 신청이 완료되었습니다. 노래이름 : ${ctx.request.body.song_name}`);
 
     ctx.status = 200;
     ctx.body = {
         "user_id" : user.user_id
     }
-}
+};
 
 export const SongList = async (ctx) => {
     
@@ -68,7 +68,7 @@ export const SongList = async (ctx) => {
         ctx.status = 400;
         ctx.body = {
             "error" : "some errorcode"
-        }
+        };
         return;
     }
 
@@ -81,7 +81,7 @@ export const SongList = async (ctx) => {
 
     let userSongArray = [];
 
-    for(var i in userList) {
+    for(let i in userList) {
         const record = {
             song_id : userList[i].rs_id,
             song_name : userList[i].title,
@@ -90,7 +90,7 @@ export const SongList = async (ctx) => {
             user_id : userList[i].user_id,
             status : userList[i].status,
             created_at : userList[i].created_at
-        }
+        };
         userSongArray.push(record);
     }
 
@@ -98,7 +98,7 @@ export const SongList = async (ctx) => {
 
     let songArray = [];
 
-    for(var i in list) {
+    for(let i in list) {
         const record = {
             song_name : list[i].title,
             singer_name : list[i].artist,
@@ -106,19 +106,18 @@ export const SongList = async (ctx) => {
             user_id : list[i].user_id,
             status : list[i].status,
             created_at : list[i].created_at
-        }
+        };
         songArray.push(record);
     }
     
-    console.log(`SongList - 노래신청목록을 반환하였습니다.`)
+    console.log(`SongList - 노래신청목록을 반환하였습니다.`);
 
     ctx.status = 200;
     ctx.body = {
         "user_song_list" : userSongArray,
         "song_list" : songArray
     }
-
-}
+};
 export const SongModify = async (ctx) => {
 
     const modUser = await decodeToken(ctx.header.token);
@@ -128,16 +127,16 @@ export const SongModify = async (ctx) => {
         }
     });
 
-    if (modUser.user_id != reservedSong.user_id) {
-        console.log(`SongModify - 수정하려는 사람이 노래를 신청한 사람과 동일하지 않습니다.`)
+    if (modUser.user_id !== reservedSong.user_id) {
+        console.log(`SongModify - 수정하려는 사람이 노래를 신청한 사람과 동일하지 않습니다.`);
         
         ctx.status = 400;
         ctx.body = {
             "error" : "some errorcode"
-        }
+        };
         return;
     }
-    //body에서 노래제목 등 받아오기
+    // body에서 노래제목 등 받아오기
 
     // await req_song.update( ctx.request.body, {
     //     where: {rs_id: ctx.request.query.song_id}
@@ -151,7 +150,8 @@ export const SongModify = async (ctx) => {
     ctx.body = {
         "user_id" : modUser.user_id
     }
-}
+};
+
 export const SongDelete = async (ctx) => {
     const delUser = await decodeToken(ctx.header.token);
 
@@ -162,23 +162,23 @@ export const SongDelete = async (ctx) => {
     });
 
     //취소하려는 사람과 요청한 사람이 일치하는가?
-    if(delUser.user_id != reservedSong.user_id) {
-        console.log(`SongDelete - 취소하려는 사람과 신청한 사람이 일치하지 않습니다.`)
+    if(delUser.user_id !== reservedSong.user_id) {
+        console.log(`SongDelete - 취소하려는 사람과 신청한 사람이 일치하지 않습니다.`);
        
         ctx.status = 400;
         ctx.body = {
             error : "some errorcode"
-        }
+        };
         return;
     }
 
-    if (reservedSong.status != "대기"){
-        console.log(`SongDelete - 현재 상태가 대기중이 아닙니다.`)
+    if (reservedSong.status !== "대기"){
+        console.log(`SongDelete - 현재 상태가 대기중이 아닙니다.`);
         
         ctx.status = 400;
         ctx.body = {
             "error" : "some errorcode"
-        }
+        };
         return;
     }
 
@@ -190,4 +190,4 @@ export const SongDelete = async (ctx) => {
     ctx.body = {
         "user_id" : delUser.user_id
     }
-} 
+};
