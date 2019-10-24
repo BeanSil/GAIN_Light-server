c1c82c7c3652d01e7052f8727f1d14cae4bc2038
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-10-24 22:50:20 +0900

    [UPDATE] Merge

--- src/api/post/index.js
+++ src/api/post/index.js
@@ -5,11 +5,13 @@ import auth from '../auth';
 
 const post = new Router();
 
+post.get('/getboard',getBoard);
+post.get('/board',getBoard); // new
 post.post('/board',uploadBoard);
-post.post('/comment',uploadcomment);
 // post.get('/board/:board_id', GetPost); // TODO: Make
 // post.put('/board/:board_id', UpdatePost); // TODO: Make
 post.delete('/board/:board_id', DeletePost);
+post.post('/comment',uploadcomment);
 post.post('/comment/:board_id',uploadcomment); // new
 // post.get('/comment/:board_id/:comment_id', DeleteComment); // TODO: Make
 // post.put('/comment/:board_id/:comment_id', DeleteComment); // TODO: Make
@@ -20,9 +22,6 @@ post.post('/board_likability',board_res); // 설명 필요
 post.post('/board_com_likability',board_com_res); // 설명 필요
 post.post('/board_data',BoardData); // 설명 필요
 
-post.get('/getboard',getBoard);
-
-post.get('/board',getBoard); // new
 
 
 export default post;
