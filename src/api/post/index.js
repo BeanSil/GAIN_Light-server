21605f3590782931010ef81b5ddfdc99bcf7cfe6
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-11-04 02:22:14 +0900

    [UPDATE] Remove Completed TODOs

--- src/api/post/index.js
+++ src/api/post/index.js
@@ -16,16 +16,16 @@ import auth from '../auth';
 
 const post = new Router();
 
-post.get('/board/:kind',GetBoardList); // TODO: Make
+post.get('/board/:kind',GetBoardList);
 post.post('/board/:kind',uploadBoard);
 post.get('/board/:kind/:board_id', GetPost);
-post.put('/board/:kind/:board_id', UpdatePost); // TODO: Make
+post.put('/board/:kind/:board_id', UpdatePost);
 post.delete('/board/:kind/:board_id', DeletePost);
 post.post('/board/:kind/:board_id/like', board_res);
-post.post('/board/:kind/:board_id/link',BoardData); // 설명 필요
+post.post('/board/:kind/:board_id/link',BoardData);
 
-post.get('/comment/:board_id',GetAllComment); // TODO: Make
-post.post('/comment/:board_id',uploadComment); // new
+post.get('/comment/:board_id',GetAllComment);
+post.post('/comment/:board_id',uploadComment);
 // post.get('/comment/:board_id/:comment_id', GetComment); // TODO: Make
 // post.put('/comment/:board_id/:comment_id', UpdateComment); // TODO: Make
 post.post('/comment/:board_id/:comment_id/like', board_com_res);
